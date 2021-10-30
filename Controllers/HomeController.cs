35b1586f99dce906887be714511edf53ebb6ab8d
Mehmetulas98 <44477481+Mehmetulas98@users.noreply.github.com>
2021-10-30 15:29:07 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CookiesAndSessionsDemo.Models;
+using RestSharp;
+using Microsoft.AspNetCore.Http;
+
+namespace CookiesAndSessionsDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+         
+        public IActionResult Index()
+        {
+
+
+            //CookieOptions cookie = new CookieOptions();
+
+            //cookie.Expires = DateTime.Now.AddYears(10);
+
+            //HttpContext.Response.Cookies.Append("username", "mehmet", cookie);
+            //HttpContext.Session.SetString("FirstSession", "SessionInfo");
+
+            var test = HttpContext.Session.GetString("FirstSession");
+            ViewBag.veri = test;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CookiesAndSessionsDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
